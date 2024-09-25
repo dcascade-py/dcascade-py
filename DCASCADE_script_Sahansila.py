b562853d0125f0414f1061cef2842cf2f58252b2
GitHubShrsetha <143914900+GitHubShrsetha@users.noreply.github.com>
2024-09-25 09:55:58 +0200

    new changes using SAFE
    new changes using SAFE

--- DCASCADE_script_Sahansila.py
+++ DCASCADE_script_Sahansila.py
@@ -40,8 +40,21 @@ import os
 
 
 
-
 '''user defined input data'''
+
+
+
+# #-------River shape files 
+# path_river_network = 'C:\\Users\\user1\\Documents\\dcascade_py\\Input\\input_trial\\'
+# name_river_network = 'River_network.shp'
+
+# #--------Q files
+# path_q = 'C:\\Users\\user1\\Documents\\dcascade_py\\Input\\input_trial\\'
+# # csv file that specifies the water flows in m3/s as a (nxm) matrix, where n = number of time steps; m = number of reaches (equal to the one specified in the river network)
+# name_q = 'Q_Vjosa.csv' 
+
+# #--------path to the output folder
+# path_results = "C:\\Users\\user1\\Documents\\Po_local\\validation\\cascade_results\\"
 
 
 #-------River shape files 
@@ -54,10 +67,12 @@ path_q = 'C:\\Sahansila\\cascade\\input\\Discharge\\'
 name_q = 'Po_Qdaily_3y.csv' 
 
 #--------path to the output folder
-path_results = "C:\\Sahansila\\cascade\\cascade_results\\"
+path_results = "C:\\Sahansila\\cascade\\cascade_results_old_script\\"
+
 
 
 #--------Parameters of the simulation
+
 
 #---Sediment classes definition 
 # defines the sediment sizes considered in the simulation
@@ -65,24 +80,21 @@ path_results = "C:\\Sahansila\\cascade\\cascade_results\\"
 sed_range = [-8, 5]  # range of sediment sizes - in Krumbein phi (φ) scale (classes from coarse to fine – e.g., -9.5, -8.5, -7.5 … 5.5, 6.5). 
 n_classes = 6        # number of classes
 
-
 #---Timescale 
 timescale = 365 # days 
-ts_length = 60*60*24 # length of timestep in seconds - 60*60*24 = daily; 60*60 = hourly
-
 
 #---Change slope or not
 update_slope = False #if False: slope is constant, if True, slope changes according to sediment deposit
 
 #---Initial layer sizes
 deposit_layer = 100000   # Initial deposit layer [m]. Warning: will overwrite the deposit column in the ReachData file
-eros_max = 1            # Maximum depth (threshold) that can be eroded in one time step (here one day), in meters. 
+AL_max = 0.3              # Maximum depth that can be eroded in one time step (here one day), in meters
 
-#---Storing Deposit layer
-save_dep_layer = 'never' #'yearly', 'always', 'never'.  Choose to save or not, the entire time deposit matrix
 
 #---Others
 roundpar = 0 #mimimum volume to be considered for mobilization of subcascade (as decimal digit, so that 0 means not less than 1m3; 1 means no less than 10m3 etc.)
+
+
 
 
 ################ MAIN ###############
@@ -92,8 +104,6 @@ ReachData = gpd.GeoDataFrame.from_file(path_river_network + name_river_network) 
 
 # Define the initial deposit layer per each reach in [m3/m]
 ReachData['deposit'] = np.repeat(deposit_layer, len(ReachData))
-
-
 
 # Read/define the water discharge 
 # but first, we check automatically the delimiter (; or ,) and if Q file has headers or not:
@@ -134,7 +144,7 @@ print(max(ReachData['D84'])*1000, ' must be lower than ',  np.percentile(dmi,90,
 
 n_reaches = len(ReachData)
 # External sediment for all reaches, all classes and all timesteps 
-Qbi_input = np.zeros((timescale,n_reaches,n_classes))
+Qbi_input = [np.zeros((n_reaches,n_classes)) for _ in range(timescale)]
 
 # Define input sediment load in the deposit layer
 deposit = ReachData.deposit*ReachData.Length
@@ -143,28 +153,25 @@ deposit = ReachData.deposit*ReachData.Length
 Fi_r,_,_ = GSDcurvefit( ReachData.D16, ReachData.D50, ReachData.D84 , psi) 
 
 # Initialise deposit layer 
-Qbi_dep_in = np.zeros((n_reaches,1,n_classes))
+Qbi_dep_in = [np.zeros((1,n_classes)) for _ in range(n_reaches)] 
 for n in range(len(ReachData)):
     Qbi_dep_in[n] = deposit[n]*Fi_r[n,:]
+    
+# to add deposit layer at a given reach 
+#row = np.array(range(n_classes)).reshape(1,n_classes)
+#Qbi_dep_in[0] = np.append(Qbi_dep_in[0],row,axis= 0)
 
-# Formula selection
-# indx_tr_cap , indx_partition, indx_flo_depth, indx_slope_red = read_user_input()
-# If you want to fix indexes, comment the line above and fix manually the indexes
-indx_tr_cap = 3 # E and H
-indx_partition = 2 # BMF
-indx_flo_depth = 1 # Manning
-indx_slope_red = 1 # None
 # Call dcascade main
-data_output, extended_output = DCASCADE_main(indx_tr_cap , indx_partition, indx_flo_depth, indx_slope_red,
-                                             ReachData, Network, Q, Qbi_input, Qbi_dep_in, timescale, psi,
-                                             roundpar, update_slope, eros_max, save_dep_layer, ts_length)
+data_output, extended_output = DCASCADE_main(ReachData, Network, Q, Qbi_input, Qbi_dep_in, timescale, psi, roundpar, update_slope, AL_max) 
 
 # Exclude variables not included in the plotting yet (sediment divided into classes)
 data_output_t = copy.deepcopy(data_output)
 variable_names = [data for data in data_output_t.keys() if data.endswith('per class [m^3/s]')]
 for item in variable_names: 
     del data_output_t[item]
-    
+
+# ## Plot results 
+# keep_slider = dynamic_plot(data_output_t, ReachData, psi)
 
 # Save results as pickled files     
 import pickle 
@@ -172,7 +179,7 @@ import pickle
 if not os.path.exists(path_results):   #does the output folder exist ?   
     os.makedirs(path_results)          # if not, create it.
     
-name_file = path_results + 'data_output_old_shp.p'
+name_file = path_results + 'data_output_old_script_Al_03.p'
 pickle.dump(data_output, open(name_file , "wb"))  # save it into a file named save.p
 
 #name_file_ext = path_results + 'save_all_ext.p'
--- Generating_random_numbers_SAFEtoolbox.py
+++ Generating_random_numbers_SAFEtoolbox.py
@@ -10,23 +10,24 @@ import numpy as np
 import geopandas as gpd
 import scipy.stats as st
 import pandas as pd
-from safepython.sampling import AAT_sampling # module to perform the input sampling
 
-# Read the shape file of the river network
-path_river_network = "E:\\UNIPD\\shp_file_slopes_hydro_and_LR\\"
-name_river_network = "Po_river_network.shp"
-ReachData = gpd.GeoDataFrame.from_file(path_river_network + name_river_network) #read shapefile from shp format
+
+#-------River shape files 
+path_river_network = "C:\\Sahansila\\cascade\\input\\shp_file_slopes_hydro_and_LR\\01-shp_corrected_names\\"
+name_river_network = 'Po_river_network.shp'
+ReachData = gpd.GeoDataFrame.from_file(path_river_network + name_river_network) #read shapefile from shp formatfrom safepython.sampling import AAT_sampling # module to perform the input sampling
+
     
 # Number of uncertain parameters subject to SA
 M = 2
 
 
-# Parameter ranges 
-xmin = [0.5, 0.5] #uniform distribution
-xmax = [1, 1.5] #uniform distrbution
+# # Parameter ranges 
+# xmin = [0.5, 0.5] #uniform distribution
+# xmax = [1, 1.5] #uniform distrbution
 
-# mean = [1, 1] # normal distribution
-# sd = [0.1, 0.5] # normal distribution 
+mean = [0, 0] # normal distribution
+sd = [0.25, 0.25] # normal distribution 
 
 # Name of parameters (will be used to customize plots):
 X_Labels = ['Wac', 'slope']
@@ -42,54 +43,80 @@ distr_fun = st.uniform # uniform distribution
    
 distr_par = [np.nan] * M
 for i in range(M):
-    distr_par[i] = [xmin[i], xmax[i] - xmin[i]]
-    # distr_par[i] = [mean[i], sd[i]]
+    # distr_par[i] = [xmin[i], xmax[i] - xmin[i]]
+    distr_par[i] = [mean[i], sd[i]]
 
 
 #sampling inputs space
 samp_strat = 'lhs' # Latin Hypercube  
 #Options: 'rsu': random uniform
          #'lhs': latin hypercube
-N = 10 #  Number of samples
+N = 200 #  Number of samples
 X = AAT_sampling(samp_strat, M, distr_fun, distr_par, N)
 # X contains the array of data with random nubers generated with which the parameters in reachdata will be multiplied, replaced or added
 
 #Give the location to save the reachdata modified
-path_ReachData = "E:\\cascade\\ReachData_for_sensitivity\\ReachData_test\\"
+path_ReachData = "C:\\sahansila\\cascade\\SAFE_output\\test\\"
 
-# Run the iterative loop for the range of number of samples and change the parameters using random numbers in X 
-#and save the modified reach data for every loop
+# # Run the iterative loop for the range of number of samples and change the parameters using random numbers in X 
+# #and save the modified reach data for every loop
+# for i in range(N):
+#     ReachData_modified = ReachData.copy() 
+#     ReachData_modified['Wac'] = ReachData_modified['Wac'] * X[i, 0]
+#     ReachData_modified['Slope'] = ReachData_modified['Slope'] * X[i, 1]
+    
+#     # Save the modified Reachdata GeoDataFrame with a useful name
+#     output_filename = f"ReachData_modified_{i+1}.shp"
+#     ReachData_modified.to_file(path_ReachData + output_filename)
+
+#     print(f"Saved {output_filename}")
+    
+# # for check if the numbers in the X corresponds to the values in the modified reach data 
+# # read the specific shape file and check the values  
+# name_ReachData = "ReachData_modified_5.shp"
+# ReachData_5 = gpd.GeoDataFrame.from_file(path_ReachData + name_ReachData) #read shapefile from shp format
+
+
+# #Save the X values in excel file
+# path_reachdata = "E:\\cascade\\ReachData_Xvalues\\"
+
+# # Create an Excel writer object
+# output_excel_path = path_reachdata + "ReachData_Xvalues_test.xlsx"
+# with pd.ExcelWriter(output_excel_path, engine='openpyxl') as writer:
+#     # Save the X array to a separate sheet
+#     X_df = pd.DataFrame(X, columns=X_Labels)
+#     # Add an index column starting from 1 to N
+#     X_df.insert(0, 'Index', range(1, len(X_df) + 1))
+#     X_df.to_excel(writer, sheet_name='Parameter_Values', index=False)
+
+# DataFrame to store the X values used for each model run
+X_used_df = pd.DataFrame(columns=['Model_Run', 'Wac_Multiplier', 'Slope_Multiplier'])
+
+
+# Iterate through N samples and modify each row in 'Wac' and 'Slope' with different values from X
 for i in range(N):
     ReachData_modified = ReachData.copy() 
-    ReachData_modified['Wac'] = ReachData_modified['Wac'] * X[i, 0]
-    ReachData_modified['Slope'] = ReachData_modified['Slope'] * X[i, 1]
     
-    # Save the modified Reachdata GeoDataFrame with a useful name
+    # Element-wise multiplication for each row in 'Wac' with different values in X[:, 0]
+    Wac_random_values = np.random.choice(X[:, 0], len(ReachData_modified))
+    ReachData_modified['Wac'] = ReachData_modified['Wac'] * Wac_random_values
+    
+    # Element-wise multiplication for each row in 'Slope' with different values in X[:, 1]
+    Slope_random_values = np.random.choice(X[:, 1], len(ReachData_modified))
+    ReachData_modified['Slope'] = ReachData_modified['Slope'] * Slope_random_values
+    
+    # Save the modified ReachData GeoDataFrame with a useful name
     output_filename = f"ReachData_modified_{i+1}.shp"
     ReachData_modified.to_file(path_ReachData + output_filename)
 
-    print(f"Saved {output_filename}")
+    # Save the unique X values used for 'Wac' and 'Slope' in this model run
+    X_used_df = X_used_df.append({'Model_Run': i+1, 
+                                  'Wac_Multiplier': Wac_random_values[0],  # Saving one value if random choice for all rows was the same
+                                  'Slope_Multiplier': Slope_random_values[0]}, 
+                                 ignore_index=True)
+
+# Save the X values DataFrame to a CSV file
+X_used_df.to_csv(path_ReachData + "X_values_used.csv", index=False)
+
     
-# for check if the numbers in the X corresponds to the values in the modified reach data 
-# read the specific shape file and check the values  
-name_ReachData = "ReachData_modified_5.shp"
-ReachData_5 = gpd.GeoDataFrame.from_file(path_ReachData + name_ReachData) #read shapefile from shp format
-
-
-#Save the X values in excel file
-path_reachdata = "E:\\cascade\\ReachData_Xvalues\\"
-
-# Create an Excel writer object
-output_excel_path = path_reachdata + "ReachData_Xvalues_test.xlsx"
-with pd.ExcelWriter(output_excel_path, engine='openpyxl') as writer:
-    # Save the X array to a separate sheet
-    X_df = pd.DataFrame(X, columns=X_Labels)
-    # Add an index column starting from 1 to N
-    X_df.insert(0, 'Index', range(1, len(X_df) + 1))
-    X_df.to_excel(writer, sheet_name='Parameter_Values', index=False)
-
-
-
-
-
 
