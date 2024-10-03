1675e91a0098a3421284e5d5252c1cd276ea8d5a
Diane Doolaeghe <119675415+didoolaeghe@users.noreply.github.com>
2024-10-03 14:47:54 +0200

    Reputing in good format (due to previous conflict)

--- DCASCADE_loop.py
+++ DCASCADE_loop.py
@@ -55,24 +55,73 @@ class ReachData:
         self.el_fn = geodataframe['el_FN'].astype(float).values
         self.el_tn = geodataframe['el_TN'].astype(float).values
 
+
         # Optional attributes
-        self.reach_id = geodataframe['reach_id'].values if 'reach_id' in geodataframe.columns else np.nan
-        self.id = geodataframe['Id'].values if 'Id' in geodataframe.columns else np.nan
-        self.q = geodataframe['Q'].values if 'Q' in geodataframe.columns else np.nan
-        self.wac_bf = geodataframe['Wac_BF'].values if 'Wac_BF' in geodataframe.columns else np.nan
-        self.D90 = geodataframe['D90'].values if 'D90' in geodataframe.columns else np.nan
-        self.s_lr_gis = geodataframe['S_LR_GIS'].values if 'S_LR_GIS' in geodataframe.columns else np.nan
-        self.tr_limit = geodataframe['tr_limit'].values if 'tr_limit' in geodataframe.columns else np.nan
-        self.x_fn = geodataframe['x_FN'].values if 'x_FN' in geodataframe.columns else np.nan
-        self.y_fn = geodataframe['y_FN'].values if 'y_FN' in geodataframe.columns else np.nan
-        self.x_tn = geodataframe['x_TN'].values if 'x_TN' in geodataframe.columns else np.nan
-        self.y_tn = geodataframe['y_TN'].values if 'y_TN' in geodataframe.columns else np.nan
-        self.ad = geodataframe['Ad'].values if 'Ad' in geodataframe.columns else np.nan
-        self.direct_ad = geodataframe['directAd'].values if 'directAd' in geodataframe.columns else np.nan
-        self.strO = geodataframe['StrO'].values if 'StrO' in geodataframe.columns else np.nan
-        self.deposit = geodataframe['deposit'].values if 'deposit' in geodataframe.columns else np.nan
-        self.geometry = geodataframe['geometry'].values if 'geometry' in geodataframe.columns else np.nan
-        
+        if 'reach_id' in geodataframe.columns:
+            self.reach_id = geodataframe['reach_id'].values
+        else:
+            self.reach_id = np.nan
+        if 'Id' in geodataframe.columns:
+            self.id = geodataframe['Id'].values
+        else:
+            self.id = np.nan
+        if 'Wac_BF' in geodataframe.columns:
+            self.wac_bf = geodataframe['Wac_BF'].values
+        else:
+            self.wac_bf = np.nan
+        if 'D90' in geodataframe.columns:
+            self.D90 = geodataframe['D90'].values
+        else:
+            self.D90 = np.nan
+        if 'S_LR_GIS' in geodataframe.columns:
+            self.s_lr_gis = geodataframe['S_LR_GIS'].values
+        else:
+            self.s_lr_gis = np.nan
+        if 'Q' in geodataframe.columns:
+            self.Q = geodataframe['Q'].values
+        else:
+            self.Q = np.nan
+        if 'tr_limit' in geodataframe.columns:
+            self.tr_limit = geodataframe['tr_limit'].values
+        else:
+            self.tr_limit = np.nan
+        if 'x_FN' in geodataframe.columns:
+            self.x_fn = geodataframe['x_FN'].values
+        else:
+            self.x_fn = np.nan
+        if 'y_FN' in geodataframe.columns:
+            self.y_fn = geodataframe['y_FN'].values
+        else:
+            self.y_fn = np.nan
+        if 'x_TN' in geodataframe.columns:
+            self.x_tn = geodataframe['x_TN'].values
+        else:
+            self.x_tn = np.nan
+        if 'y_TN' in geodataframe.columns:
+            self.y_tn = geodataframe['y_TN'].values
+        else:
+            self.y_tn = np.nan
+        if 'Ad' in geodataframe.columns:
+            self.ad = geodataframe['Ad'].values
+        else:
+            self.ad = np.nan        
+        if 'StrO' in geodataframe.columns:
+            self.strO = geodataframe['StrO'].values
+        else:
+            self.strO = np.nan
+        if 'deposit' in geodataframe.columns:
+            self.deposit = geodataframe['deposit'].values
+        else:
+            self.deposit = np.nan
+        if 'geometry' in geodataframe.columns:
+            self.geometry = geodataframe['geometry'].values
+        else:
+            self.geometry = np.nan
+        if 'directAd' in geodataframe.columns:
+            self.direct_ad = geodataframe['directAd'].values
+        else:
+            self.direct_ad = np.nan            
+
     def sort_values_by(self, sorting_array):
         """
         Function to sort the Reaches by the array given in input.
