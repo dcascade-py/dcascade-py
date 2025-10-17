9e4499dcb27298384ae160f59865dde225ba623e
Diane Doolaeghe <119675415+didoolaeghe@users.noreply.github.com>
2025-10-17 16:15:59 +0200

    Adding discharge

--- src/dcascade.py
+++ src/dcascade.py
@@ -298,6 +298,7 @@ class DCASCADE:
                        'D50 active layer [m]': SedimSys.D50_al.astype(np.float32),
                        'Direct connectivity [m^3]': direct_connectivity.astype(np.float32),
                        'Transport capacity [m^3]': transport_capacity.astype(np.float32),
+                       'Discharge [m^3/s]': Q,
 
                        # TODO: 'Touch erosion max': touch_eros_max,
                         }
