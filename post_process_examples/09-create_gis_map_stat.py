d65dc46a23670af90eb8f4a944a9adec92c6476f
Fabio José Andres Schneider <33283311+fjaschneider@users.noreply.github.com>
2025-10-27 17:42:19 +0100

    Update 09-create_gis_map_stat.py

--- post_process_examples/09-create_gis_map_stat.py
+++ post_process_examples/09-create_gis_map_stat.py
@@ -23,8 +23,9 @@ trib = '100000'
 
 
 #---------------------Path to the extended pickle output
-path = f'K:/Labo/20_OSR/Rhone_Fabio_Schneider/DCASCADE_res{years}_{eq}_noDams/'
+path = f'K:/Labo/20_OSR/Rhone_Fabio_Schneider/DCASCADE_res{years}_{eq}_Sand/'
 name_simu = f'DCASCADE_res{years}_{eq}'
+name_simu_ext = f'DCASCADE_res{years}_{eq}_ext'
 
 #---River shape files
 path_river_network = Path('../inputs/Rhone_river/Network/DCASCADE/')
@@ -32,6 +33,7 @@ path_river_network = Path('../inputs/Rhone_river/Network/DCASCADE/')
 name_river_network = 'Network_dcascade.shp'
 filename_river_network = path_river_network / name_river_network
 
+# open simulate
 with open( path + name_simu + '.p' , "rb") as readF:
     a = pickle.load(readF)
 
@@ -41,6 +43,7 @@ aVin = a['Volume in [m^3]']
 aDep = a['Deposited [m^3]']
 aTr = a['Transport capacity [m^3]']
 aQ = a['Discharge [m^3/s]']
+aQh = a['Flow depth [m]']
 
 
 aSed_budget = np.sum(aSed_budget, axis=0)
@@ -49,6 +52,7 @@ aVin = np.sum(aVin, axis=0)
 aDep = np.sum(aDep, axis=0)
 aTr = np.sum(aTr, axis=0)
 aQ = np.max(aQ, axis=0)
+aQh = np.max(aQh, axis=0)
 
 df_Sed_budget = pd.DataFrame({'SedBudget': aSed_budget})
 df_Vout = pd.DataFrame({'Vout': aVout})
@@ -56,6 +60,7 @@ df_Vin = pd.DataFrame({'Vin': aVin})
 df_Dep = pd.DataFrame({'Dep': aDep})
 df_Tr = pd.DataFrame({'Tr': aTr})
 df_Qc = pd.DataFrame({'Qc': aQ})
+df_Qh = pd.DataFrame({'Qh': aQh})
 
 # Process discharge stats
 df_discharge = pd.read_csv(f'../inputs/Rhone_river/discharge_martinez/Q_{years}_dam.csv')
@@ -81,10 +86,11 @@ df_res_output = pd.concat([
     df_Vin,
     df_Dep,
     df_Tr,
-    df_Qc
+    df_Qc,
+    df_Qh
 ], axis=1)
 
-csv_path = path + f'df_DCASCADE_res{years}_{eq}_noDams.csv'
+csv_path = path + f'df_DCASCADE_res{years}_{eq}_Sand.csv'
 df_res_output.to_csv(csv_path, index=False)
 
 gdf_rhone = gpd.read_file(filename_river_network)
@@ -93,5 +99,55 @@ gdf_cascade = pd.concat([gdf_rhone, df_res_cascade], axis=1)
 gdf_cascade.rename(columns={'fid': 'id'}, inplace=True)
 
 # gdf_cascade.to_file(path_base + f'df_res_output_ext_{year}.shp', driver='ESRI Shapefile')
-gdf_cascade.to_file(path + f'DCASCADE_res{years}_{eq}_noDams.gpkg', driver='GPKG')
+gdf_cascade.to_file(path + f'DCASCADE_res{years}_{eq}_Sand.gpkg', driver='GPKG')
+
+
+
+# open simulate extend
+with open( path + name_simu_ext + '.p' , "rb") as readF_ext:
+    a_ext = pickle.load(readF_ext)
+
+aSed_budget_class_ext = a['Sediment budget per class [m^3]']
+aVout_class_ext = a['Volume out per grain sizes [m^3]']
+aVin_class_ext = a['Volume in per grain sizes [m^3]']
+aDep_class_ext = a['Deposited per grain sizes [m^3]']
+aTr_class_ext = a['Tr_cap per class [m^3]']
+
+aVout_class_ext = np.sum(aVout_class_ext, axis=0)
+aTr_class_ext = np.sum(aTr_class_ext, axis=0)
+aVin_class_ext = np.sum(aVin_class_ext, axis=0)
+aDep_class_ext = np.sum(aDep_class_ext, axis=0)
+aSed_budget_class_ext = np.sum(aSed_budget_class_ext, axis=0)
+
+df_Vout_class_ext = pd.DataFrame(aVout_class_ext)
+df_Tr_class_ext = pd.DataFrame(aTr_class_ext)
+df_Vin_class_ext = pd.DataFrame(aVin_class_ext)
+df_Dep_class_ext = pd.DataFrame(aDep_class_ext)
+df_Sed_budget_class_ext = pd.DataFrame(aSed_budget_class_ext)
+
+for df, prefix in [
+    (df_Vout_class_ext, 'Vout'),
+    (df_Tr_class_ext, 'Tr_cap'),
+    (df_Vin_class_ext, 'Vin'),
+    (df_Dep_class_ext, 'Dep'),
+    (df_Sed_budget_class_ext, 'Sed_bud')
+]:
+    # df[f'{prefix}_boulder'] = df.iloc[:, [0,1,2,3]].sum(axis=1)
+    df[f'{prefix}_gravel'] = df.iloc[:, [4,5,6,7,8]].sum(axis=1)
+    df[f'{prefix}_sand'] = df.iloc[:, [9,10,11,12,13]].sum(axis=1)
+    df[f'{prefix}_Tot_classes'] = df.iloc[:, :14].sum(axis=1)
+    
+for df, prefix in [
+    (df_Tr_class_ext, 'Tr_cap'),
+]:
+    # df[f'{prefix}_boulder'] = df.iloc[:, [0,1,2,3]].mean(axis=1)
+    df[f'{prefix}_gravel'] = df.iloc[:, [4,5,6,7,8]].mean(axis=1)
+    df[f'{prefix}_sand'] = df.iloc[:, [9,10,11,12,13]].mean(axis=1)
+    df[f'{prefix}_Tot_classes'] = df.iloc[:, :14].mean(axis=1)    
+
+df_Vout_class_def = df_Vout_class_ext.iloc[:, -4:]
+df_Tr_class_def = df_Tr_class_ext.iloc[:, -4:]
+df_Vin_class_def = df_Vin_class_ext.iloc[:, -4:]
+df_Dep_class_def = df_Dep_class_ext.iloc[:, -4:]
+df_Sed_budget_class_def = df_Sed_budget_class_ext.iloc[:, -4:]
 
