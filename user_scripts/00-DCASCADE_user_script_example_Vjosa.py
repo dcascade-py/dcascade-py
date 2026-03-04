ab43c8a43672e31e2b4c88ef4a7560eee5717c50
Diane Doolaeghe <119675415+didoolaeghe@users.noreply.github.com>
2026-03-04 12:54:11 +0100

    Update 00-DCASCADE_user_script_example_Vjosa.py
    deposit x width

--- user_scripts/00-DCASCADE_user_script_example_Vjosa.py
+++ user_scripts/00-DCASCADE_user_script_example_Vjosa.py
@@ -177,7 +177,7 @@ check_sediment_sizes(reach_data, dmi)
 external_inputs = np.zeros((timescale, reach_data.n_reaches, n_classes))
 
 # Define input sediment load in the deposit layer
-deposit = reach_data.deposit * reach_data.length
+deposit = reach_data.deposit * reach_data.length * reach_data.wac
 
 # Define initial sediment fractions per class in each reaches, using a Rosin distribution
 Fi_r, _, _ = GSDcurvefit(reach_data.D16, reach_data.D50, reach_data.D84, psi)
