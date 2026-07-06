4214f812e2ee7196c3ebe9fe81903264360520ea
Diane Doolaeghe <119675415+didoolaeghe@users.noreply.github.com>
2026-07-06 16:32:57 +0200

    save Po inputs used here

--- src/main.py
+++ src/main.py
@@ -36,7 +36,7 @@ def DCASCADE_main(reach_data, network, Q, psi, timescale, ts_length, al_depth,
                   force_pass_external_inputs = False,
 
                   passing_cascade_in_outputs = True,
-                  passing_cascade_in_trcap = False,
+                  passing_cascade_in_trcap = True,
                   time_lag_for_mobilised = False):
 
 
